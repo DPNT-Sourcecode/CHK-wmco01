003c9d6e593c4a4c66bf108314bad67ba2429715
sbx_user1051 <sbx_user1051@169.254.0.107>
2025-11-20 23:15:20 +0000

    Thu Nov 20 23:15:20 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,22 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        raise NotImplementedError()
+        if skus is None or skus == "":
+            return -1
+        prices = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15
+        }
+        offers = {
+            "A": {3, 150},
+            "B": {2, 45}
+        }
+        counts = {}
+        for ch in skus:
+            if ch not in prices:
+                return -1
+            counts[ch] = counts.get(ch, 0) + 1
+        total 
+
