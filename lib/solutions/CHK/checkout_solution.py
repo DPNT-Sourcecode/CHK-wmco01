a03f33e1c5fcb6accc6b018b79bd68c200d0457b
sbx_user1051 <sbx_user1051@169.254.0.107>
2025-11-20 23:18:20 +0000

    Thu Nov 20 23:18:20 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -12,13 +12,28 @@ class CheckoutSolution:
             "D": 15
         }
         offers = {
-            "A": {3, 150},
-            "B": {2, 45}
+            "A": (3, 150),
+            "B": (2, 45)
         }
         counts = {}
         for ch in skus:
             if ch not in prices:
                 return -1
             counts[ch] = counts.get(ch, 0) + 1
-        total 
+        total = 0
+        for item, cnt in counts.items():
+            if item in offers:
+                offer_quality, offer_price = offers[item]
+                offer_times = cnt // offer_quality
+                remainder = cnt % offer_quality
+                total += offer_times * offer_price
+                total += remainder * prices[item]
+            else:
+                total += cnt * prices[item]
+        return total
+print(CheckoutSolution.checkout("A"))
+print(CheckoutSolution.checkout("AAA"))
+print(CheckoutSolution.checkout("AAA"))
+
+
 
